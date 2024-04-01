a4d80de5dbfd644867642d68d598a410b6352811
Kevin Suñer <ksuner@pm.me>
2024-04-01 13:33:20 +0200

    added init command to create req directories, and with optional theme download from github

--- cmd/init.go
+++ cmd/init.go
@@ -1,46 +1,83 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
+	"net/url"
 	"os"
+	"os/exec"
+	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+var Dir, Theme string
+
 func init() {
     rootCmd.AddCommand(initCmd)
+    initCmd.Flags().StringVar(&Dir, "dir", "", "project directory")
+    initCmd.Flags().StringVar(&Theme, "theme", "", "e.g. https://github.com/kevinsuner/elmo-erlosung.git")
+    initCmd.MarkFlagRequired("dir")
 }
 
 var initCmd = &cobra.Command{
-    Use: "init <project-name>",
+    Use: "init",
     Short: "placeholder",
     Long: `placeholder`,
     Run: initialize,
 }
 
 func initialize(cmd *cobra.Command, args []string) {
-    if len(args[0]) == 0 {
-        log.Fatal("len(args[0])", "error", "empty argument")
+    ts := time.Now()
+    log := viper.Get("Logger").(*log.Logger)
+
+    _, err := os.Stat(Dir)
+    if err != nil {
+        if !errors.Is(err, fs.ErrNotExist) {
+            log.Fatal("os.Stat", "error", err.Error())
+        }
     }
 
-    _, err := os.Stat(args[0])
-    switch {
-    case os.IsNotExist(err):
-        break
-    default:
-        log.Fatal("os.Stat", "error", err.Error())
-    }
-
-    if err = os.MkdirAll(
+    if err := os.MkdirAll(
         fmt.Sprintf(
             "%s/%s",
-            args[0],
+            Dir,
             viper.GetString("ContentPostsDir"),
         ),
         os.ModePerm,
     ); err != nil {
         log.Fatal("os.MkdirAll", "error", err.Error())
     }
+
+    if err := os.Mkdir(
+        fmt.Sprintf(
+            "%s/%s",
+            Dir,
+            viper.GetString("ThemesDir"),
+        ),
+        os.ModePerm,
+    ); err != nil {
+        log.Fatal("os.Mkdir", "error", err.Error())
+    }
+
+    if cmd.Flag("theme").Changed {
+        _, err = url.ParseRequestURI(cmd.Flag("theme").Value.String())
+        if err != nil {
+            log.Fatal("url.ParseRequestURI", "error", err.Error())
+        }
+
+        command := exec.Command("git", "clone", cmd.Flag("theme").Value.String())
+        command.Dir = fmt.Sprintf("%s/%s", Dir, viper.GetString("ThemesDir"))
+        if err := command.Run(); err != nil {
+            log.Fatal("exec.Command", "error", err.Error())
+        }
+    }
+
+    log.SetReportCaller(false)
+    log.Info("Done!", "took", fmt.Sprintf("%dms", time.Since(ts).Milliseconds()))
+    log.Info("Created project", "dir", Dir)
+    if cmd.Flag("theme").Changed { log.Info("Downloaded theme", "url", Theme) }
 }
